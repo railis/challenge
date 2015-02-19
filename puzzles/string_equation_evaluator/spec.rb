3545d7c9fce60dee29ce5f8174afb66ab9c75a2b
Dominik Sito <dominik.sito@gmail.com>
2015-02-19 03:05:33 +0100

    Fixes readme and specs

--- puzzles/string_equation_evaluator/spec.rb
+++ puzzles/string_equation_evaluator/spec.rb
@@ -1,106 +1,48 @@
 Challenge::Spec.new do
   context "when expression is invalid" do
-    context "when not closed by parentheses" do
-      let(:exp) { "1 + 2" }
+    context "when it includes invalid characters" do
+      let(:exp) { "1 + 2a" }
 
       it "raises error" do
-        expect { @solution_class.new(exp).calculate }.to raise_error
+        expect { @solution_class.new(exp).calculate }.to raise_error("Invalid character 'a'")
       end
     end
 
-    context "when there are more than two operands per expression" do
-      let(:exp) { "(1 + 2 + 3)" }
+    context "when there are not closed parentheses" do
+      let(:exp) { "((1 + 2 + 3) + 1" }
 
       it "raises error" do
-        expect { @solution_class.new(exp).calculate }.to raise_error
+        expect { @solution_class.new(exp).calculate }.to raise_error("Unmatched '('")
       end
     end
     
-    context "when pharentheses are not closed" do
-      let(:exp) { "((1 + 2) + 1" }
+    context "when parentheses are closed more than once" do
+      let(:exp) { "((1 + 2) + 1) + 4)" }
 
       it "raises error" do
-        expect { @solution_class.new(exp).calculate }.to raise_error
-      end
-    end
-
-    context "when there are missing characters" do
-      let(:exp) { "((1 + 2a) / 1" }
-
-      it "raises error" do
-        expect { @solution_class.new(exp).calculate }.to raise_error
+        expect { @solution_class.new(exp).calculate }.to raise_error("Unmatched '('")
       end
     end
   end
 
   context "when expression is valid" do
     context "allows negative" do
-      let (:exp) { "(-2 + -5)" }
+      let (:exp) { "(-2) + 1" }
 
-      it "raises error" do
-        expect(@solution_class.new(exp).calculate).to eq(-7)
+      it "returns calculated value" do
+        expect(@solution_class.new(exp).calculate).to eq(-1)
       end
     end
 
-    context "and not nested" do
-      context do
-        let(:exp) { "(1 + 2)" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(3)
-        end
+    context "handles basic ordering" do
+      it "returns calculated value based on attributes order" do
+        expect(@solution_class.new("2+2*2").calculate).to eq(6)
+        expect(@solution_class.new("10-4/2").calculate).to eq(8)
       end
 
-      context do
-        let(:exp) { "(5 - 2)" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(3)
-        end
-      end
-    end
-
-    context "and more nested" do
-      context do
-        let(:exp) { "(5 - (1 + 2))" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(2)
-        end
-      end
-
-      context do
-        let(:exp) { "((1 + 2) - 5)" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(-2)
-        end
-      end
-    end
-
-    context "and more more nested" do
-      context do
-        let(:exp) { "((5 + 7) - (1 + 2))" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(9)
-        end
-      end
-
-      context do
-        let(:exp) { "((5 - 7) - (1 + 2))" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(-5)
-        end
-      end
-
-      context do
-        let(:exp) { "(1 + ((2 + 1) + (4 - (9 - 2))))" }
-
-        it "returns calculated value" do
-          expect(@solution_class.new(exp).calculate).to eq(1)
-        end
+      it "returns calculated value based on parentheses" do
+        expect(@solution_class.new("(2+2)*2").calculate).to eq(8)
+        expect(@solution_class.new("(10-4)/2").calculate).to eq(3)
       end
     end
   end
